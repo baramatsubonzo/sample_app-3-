3749f2c58afb2bc6c668fdfa505599a0de0e7a75
baramatsubonzo <baramatsubonzo@gmail.com>
2018-09-02 10:15:18 +0900

    静的ページの作成

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,34 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample app"
+  end
+
+
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
+    assert_select "title" , "ホーム | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
+    assert_select "title" , "アバウト | #{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
+    assert_select "title" , "ヘルプ | #{@base_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_url
     assert_response :success
+    assert_select "title" , "コンタクト | #{@base_title}"
   end
 
 end
